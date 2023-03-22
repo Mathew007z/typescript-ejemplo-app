eeaf95a00c2adbbfc4c91ecab83bd53198780636
Sergio <sergio.amarilla@arkanosoft.com>
2023-03-22 15:50:26 -0300

    first commit ts react

---
+++ src/components/types.d.ts
@@ -0,0 +1,8 @@
+
+// Una interfaz es un contrato que tiene que tener un objeto o una clase. 
+export interface Sub {
+    nick:string,
+    avatar:string,
+    subMonths:number,
+    description?: string
+   } 
